af7505147ecedbd4c321e26de03a24f777cffcda
Palakpreet Kaur <hs960970@gmail.com>
2026-09-08 17:31:41 +0530

    Update MongoDB connection string with new credentials

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 let mongoose = require('mongoose')
-const dbPath = 'mongodb+srv://dbuser:dbpassword@mongo.vk6tb.mongodb.net/kizaTexttiles'
+const dbPath = 'mongodb+srv://user:preet123@cluster0.rnx9hye.mongodb.net/kizaTexttiles'
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.connect(dbPath, options).then(res => {
     console.log("Db Connected")
